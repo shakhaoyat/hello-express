931ff42d95a7aa80e366cecfd9a53700463f107e
shakhaoyat <shakhaoyathossain.12@gmail.com>
2026-02-02 17:53:46 +0600

    Remove vercel.json for auto-detection

--- api/index.js
+++ api/index.js
@@ -1,29 +1,29 @@
 function gcd(a, b) {
-    while (b !== 0) {
-        let temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
+      while (b !== 0) {
+            let temp = b;
+            b = a % b;
+            a = temp;
+      }
+      return a;
 }
 
 function lcm(x, y) {
-    return (x * y) / gcd(x, y);
+      return (x * y) / gcd(x, y);
 }
 
 function isNaturalNumber(value) {
-    const num = Number(value);
-    return Number.isInteger(num) && num > 0;
+      const num = Number(value);
+      return Number.isInteger(num) && num > 0;
 }
 
 module.exports = (req, res) => {
-    // Handle both root and /shakhaoyathossain_12_gmail_com
-    const { x, y } = req.query;
+      // Handle both root and /shakhaoyathossain_12_gmail_com
+      const { x, y } = req.query;
 
-    if (!isNaturalNumber(x) || !isNaturalNumber(y)) {
-        return res.status(200).send('NaN');
-    }
+      if (!isNaturalNumber(x) || !isNaturalNumber(y)) {
+            return res.status(200).send('NaN');
+      }
 
-    const result = lcm(Number(x), Number(y));
-    res.status(200).send(result.toString());
+      const result = lcm(Number(x), Number(y));
+      res.status(200).send(result.toString());
 };
--- api/shakhaoyathossain_12_gmail_com.js
+++ api/shakhaoyathossain_12_gmail_com.js
@@ -1,28 +1,28 @@
 function gcd(a, b) {
-    while (b !== 0) {
-        let temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
+      while (b !== 0) {
+            let temp = b;
+            b = a % b;
+            a = temp;
+      }
+      return a;
 }
 
 function lcm(x, y) {
-    return (x * y) / gcd(x, y);
+      return (x * y) / gcd(x, y);
 }
 
 function isNaturalNumber(value) {
-    const num = Number(value);
-    return Number.isInteger(num) && num > 0;
+      const num = Number(value);
+      return Number.isInteger(num) && num > 0;
 }
 
 module.exports = (req, res) => {
-    const { x, y } = req.query;
+      const { x, y } = req.query;
 
-    if (!isNaturalNumber(x) || !isNaturalNumber(y)) {
-        return res.status(200).send('NaN');
-    }
+      if (!isNaturalNumber(x) || !isNaturalNumber(y)) {
+            return res.status(200).send('NaN');
+      }
 
-    const result = lcm(Number(x), Number(y));
-    res.status(200).send(result.toString());
+      const result = lcm(Number(x), Number(y));
+      res.status(200).send(result.toString());
 };
