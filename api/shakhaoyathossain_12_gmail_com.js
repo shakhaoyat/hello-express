ddd25de293fc3f28c9e055624827f7dee897a490
shakhaoyat <shakhaoyathossain.12@gmail.com>
2026-02-02 17:41:42 +0600

    Fix API route with CommonJS

--- api/shakhaoyathossain_12_gmail_com.js
+++ api/shakhaoyathossain_12_gmail_com.js
@@ -1,32 +1,28 @@
-// GCD using Euclidean algorithm
 function gcd(a, b) {
-      while (b !== 0) {
-            let temp = b;
-            b = a % b;
-            a = temp;
-      }
-      return a;
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
 
-// LCM calculation
 function lcm(x, y) {
-      return (x * y) / gcd(x, y);
+    return (x * y) / gcd(x, y);
 }
 
-// Check if value is a natural number
 function isNaturalNumber(value) {
-      const num = Number(value);
-      return Number.isInteger(num) && num > 0;
+    const num = Number(value);
+    return Number.isInteger(num) && num > 0;
 }
 
-// Vercel serverless function
-export default function handler(req, res) {
-      const { x, y } = req.query;
+module.exports = (req, res) => {
+    const { x, y } = req.query;
 
-      if (!isNaturalNumber(x) || !isNaturalNumber(y)) {
-            return res.status(200).send('NaN');
-      }
+    if (!isNaturalNumber(x) || !isNaturalNumber(y)) {
+        return res.status(200).send('NaN');
+    }
 
-      const result = lcm(Number(x), Number(y));
-      res.status(200).send(result.toString());
-}
+    const result = lcm(Number(x), Number(y));
+    res.status(200).send(result.toString());
+};
